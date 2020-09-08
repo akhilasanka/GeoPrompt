9d32e286ab0925167c01daf793c9146767d3a2d1
Akhila <akhila_sanka@intuit.com>
2020-09-07 17:37:35 -0700

    backend setup with test api

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+var express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+const cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+app.use(
+	session({
+		secret: "cmpe295b_geoprompt_node",
+		resave: false, // Forces the session to be saved back to the session store, even if the session was never modified during the request
+		saveUninitialized: false, // Force to save uninitialized session to db. A session is uninitialized when it is new but not modified.
+		duration: 60 * 60 * 1000, // Overall duration of Session : 30 minutes : 1800 seconds
+		activeDuration: 5 * 60 * 1000,
+	})
+);
+
+app.use(function (req, res, next) {
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET,HEAD,OPTIONS,POST,PUT,DELETE"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+	);
+	res.setHeader("Cache-Control", "no-cache");
+	next();
+});
+
+//use cors to allow cross origin resource sharing
+app.use(cors({ origin: "http://localhost:8081", credentials: true }));
+
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+app.use(bodyParser.json());
+
+app.get("/test", function (req, res) {
+	res.send("Hello World!");
+});
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
