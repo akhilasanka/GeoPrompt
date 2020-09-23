24522a747836f51d320e323d0beb98f7089a3e10
Kavya Sri Chennoju <kavya.chennoju@sjsu.edu>
2020-09-23 11:37:00 -0700

    Mark as complete and Task History

--- Frontend/components/screens/TaskHistory.js
+++ Frontend/components/screens/TaskHistory.js
@@ -12,68 +12,50 @@ import axios from 'axios';
 import {backendBaseURL} from '../constants/Constants';
 import {Icon} from 'react-native-elements';
 
-export default class ListTaskScreen extends React.Component {
+export default class TaskHistory extends React.Component {
+static navigationOptions = ({navigation}) => ({
+    headerTitle: 'Task History',
+    headerLeft: Platform.select({
+      ios: (
+        <Icon
+          name="ios-log-out"
+          type="ionicon"
+          containerStyle={styles.icon}
+          onPress={() => navigation.navigate('LoginScreen')}
+        />
+      ),
+      android: (
+        <Icon
+          name="md-menu"
+          type="ionicon"
+          containerStyle={styles.icon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      ),
+    }),
+  });
   constructor(props) {
     super(props);
-
     this.state = {
       colors: ['aliceblue', 'azure', 'beige'],
-      tasks: [],
+      tasks: []
     };
   }
-  static navigationOptions = ({navigation}) => ({
-    headerTitle: 'Home',
-    headerLeft: () => (
-      <Icon
-        name="md-menu"
-        type="ionicon"
-        containerStyle={styles.icon}
-        onPress={() => navigation.toggleDrawer()}
-      />
-    ),
-    headerRight: () => (
-      <Icon
-        name="add-task"
-        type="material-icons"
-        containerStyle={styles.iconPlus}
-        onPress={() => navigation.navigate('AddReminderScreen')}
-      />
-    ),
-  });
   componentDidMount() {
-    var email = "a"
-    axios
-      .get(backendBaseURL + '/geoprompt/tasks?email='+ email)
-      .then((res) => {
-        const tasks = res.data.results;
-        this.setState({tasks: tasks});
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  onPressEditButton = (item) => {
-    alert('U can edit task ' + item.title);
-  };
-  setTaskComplete = (item) => {
-     axios
-          .post(backendBaseURL + '/geoprompt/taskComplete',{"itemid":item._id})
-          .then((res) => {
-            alert("Congratulations on Completing the Task!")
+      var email = "a"
+      this.subs = this.props.navigation.addListener("didFocus", () =>
             axios
-              .get(backendBaseURL + '/geoprompt/tasks?email=a')
+              .get(backendBaseURL + '/geoprompt/completedTasks?email=' + email)
               .then((res) => {
                 const tasks = res.data.results;
                 this.setState({tasks: tasks});
               })
               .catch((err) => {
                 console.log(err);
-              });
-          })
-          .catch((err) => {
-            alert(err)
-          });
-  };
+              })
+      );
+  }
+
   render() {
     if (this.state.tasks && this.state.tasks.length != 0) {
       return (
@@ -83,23 +65,11 @@ export default class ListTaskScreen extends React.Component {
               key={index}
               style={{flex: 2, backgroundColor: this.state.colors[index % 3]}}>
               <Text style={styles.title}>{item.title}</Text>
-              <Text style={styles.description}>{item.description}</Text>
-              <Text style={styles.text}>Status: {item.status}</Text>
+              { item.description ?
+              <Text style={styles.description}>{item.description}</Text> :
+              null
+              }
               <Text style={styles.text}>Category: {item.categoryName}</Text>
-              <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                  <CheckBox
-                    value={false}
-                    onValueChange={()=>this.setTaskComplete(item)}
-                    style={styles.text}
-                  />
-                  <Text>Mark as complete!</Text>
-              </View>
-            <Button
-              style={styles.editButton}
-              title="Edit Task"
-              color="lightslategrey"
-              onPress={() => this.onPressEditButton(item)}
-            />
             </TouchableOpacity>
           ))}
         </ScrollView>
@@ -107,7 +77,7 @@ export default class ListTaskScreen extends React.Component {
     } else {
       return (
         <View style={styles.NoTasks}>
-          <Text>No tasks Added!!!</Text>
+          <Text>No Completed Tasks!!!</Text>
         </View>
       );
     }
@@ -152,6 +122,7 @@ const styles = StyleSheet.create({
   editButton: {
     backgroundColor: 'lightslategrey',
     color: 'white',
+    alignItems: 'center',
   },
   icon: {
     padding: 10,
