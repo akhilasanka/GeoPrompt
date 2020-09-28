0cc31d4986b53dfa4f9078cb381a2ee81142d320
Vibha Ravindra <vibhashree.ravindra@sjsu.edu>
2020-09-27 17:02:36 -0700

    GET API to fetch tasks nearby, gives list of tasks for each nearby location

--- backend/src/routes/Notification.js
+++ backend/src/routes/Notification.js
@@ -12,6 +12,68 @@ firebase_admin.initializeApp({
   databaseURL: "https://geoprompt-notifications.firebaseio.com"
 });
 
+router.get("/tasksnearby", function(req, res){
+    const email = req.query.email;
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+    Tasks.tasks.find({ "email": email, "status": "Pending"}, function (err, results) {
+        if (err){
+            res.status(500).json({ responseMessage: err.message  });
+        } else {
+            var cat_task_map = {};
+            results.forEach(row => {
+                if(row.categoryName in foursquare_conf.category_name_id_map) {
+                    const cat_id = foursquare_conf.category_name_id_map[row.categoryName];
+                    if (!(cat_id in cat_task_map)) {
+                        cat_task_map[cat_id] = [];
+                    }
+                    cat_task_map[cat_id].push(row);
+                }
+            });
+            const categories_to_fetch = Object.keys(cat_task_map).join(",");
+            console.log("Foursquare categories to fetch: " + categories_to_fetch);
+            axios.get(foursquare_conf.venues_url, {
+                params:{
+                    client_id: foursquare_conf.client_id,
+                    client_secret: foursquare_conf.client_secret,
+                    v:foursquare_conf.version,
+                    ll:lat + "," + lon,
+                    intent: "browse",
+                    radius: foursquare_conf.radius,
+                    limit: foursquare_conf.num_results,
+                    categoryId: categories_to_fetch
+                }
+            })
+            .then(function (response) {
+                const venues = response.data["response"]["venues"];
+                var response_data = [];
+                venues.forEach(venue => {
+                    var tasks_at_venue = new Set();
+                    venue.categories.forEach(category => {
+                        const id = category.id;
+                        if(id in cat_task_map) {
+                            tasks_at_venue.add(cat_task_map[id]);
+                        }
+                    });
+                    const venue_data = {
+                        "Name": venue.name,
+                        "lat": venue.location.lat,
+                        "lon": venue.location.lng,
+                        "address": venue.location.formattedAddress[0],
+                        "tasks": Array.from(tasks_at_venue)
+                    };
+                    response_data.push(venue_data);
+                });
+                res.status(200).json({ tasks: response_data });
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.status(500).json({ responseMessage: error });
+            });
+        }
+    });
+});
+
 router.post('/reportlocation', function (req, res) {
     const email = req.body.email;
     const lat = req.body.lat;
